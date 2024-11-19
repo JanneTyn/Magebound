08a08e3fbf4acb97a12e9f3b035df09c737f3da9
Janne <jannetyni98@gmail.com>
2024-11-19 12:54:09 +0200

    jotain

--- Assets/SpellBaseEffect.cs
+++ Assets/SpellBaseEffect.cs
@@ -1,9 +1,13 @@
+using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.VFX;
 
 public class SpellBaseEffect : MonoBehaviour
 {
 
     public GameObject BasicAttackFirePrefab;
+    public VisualEffectAsset PreLoadBasicAttackFirePrefab;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -20,8 +24,10 @@ public class SpellBaseEffect : MonoBehaviour
     {
         Vector3 linedir = (targetLocation - playerLocation).normalized;
         Debug.DrawLine(playerLocation, playerLocation + linedir * 50, Color.red, Mathf.Infinity);
-
+        
         GameObject attack = Instantiate(BasicAttackFirePrefab, playerLocation, Quaternion.identity);
         attack.GetComponent<SpellProjectile>().SetProjectileDirection(targetLocation, playerLocation);
+        //var VFX = Resources.Load<VisualEffectAsset>("VFX/BasicProjectile/BasicProjectileFire");
+        //attack.GetComponent<VisualEffect>().visualEffectAsset = VFX;
     }
 }
