8a9cc97ece430c72bff87e4a7e71f78172cdbe11
Janne <jannetyni98@gmail.com>
2024-11-20 11:07:26 +0200

    rotation juttuja

--- Assets/Dash.cs
+++ Assets/Dash.cs
@@ -6,7 +6,7 @@ public class Dash : MonoBehaviour
 {
     public float dashDistance = 5;
     public float dashDuration = 0.25f;
-    bool dashIsActive = false;
+    public bool dashIsActive = false;
     public void InitializeDash(Vector3 playerLocation, Vector3 targetLocation)
     {       
          Vector3 vectorDist = targetLocation - playerLocation;
@@ -20,6 +20,7 @@ public class Dash : MonoBehaviour
     {
         var t = 0f;
         dashIsActive = true;
+        transform.LookAt(targetLocation);
         while (t < 1)
         {
             t += Time.deltaTime / dashDuration;
