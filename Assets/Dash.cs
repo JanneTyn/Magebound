deac21df535d8ad7874b28ba06b07689133843da
Janne <jannetyni98@gmail.com>
2024-11-22 10:45:40 +0200

    dash effect added

--- Assets/Dash.cs
+++ Assets/Dash.cs
@@ -7,16 +7,18 @@ public class Dash : MonoBehaviour
     public float dashDistance = 5;
     public float dashDuration = 0.25f;
     public bool dashIsActive = false;
+    public GameObject DashFirePrefab;
     public void InitializeDash(Vector3 playerLocation, Vector3 targetLocation)
     {       
          Vector3 vectorDist = targetLocation - playerLocation;
          vectorDist.Normalize();
          targetLocation = playerLocation + (dashDistance * vectorDist);
-        
-         if (!dashIsActive) StartCoroutine(DashActive(playerLocation, targetLocation));
+
+        GameObject dashEffect = Instantiate(DashFirePrefab, playerLocation, Quaternion.identity);
+        if (!dashIsActive) StartCoroutine(DashActive(playerLocation, targetLocation, dashEffect));
     }
 
-    IEnumerator DashActive(Vector3 playerLocation, Vector3 targetLocation)
+    IEnumerator DashActive(Vector3 playerLocation, Vector3 targetLocation, GameObject dashEffect)
     {
         var t = 0f;
         dashIsActive = true;
@@ -27,6 +29,7 @@ public class Dash : MonoBehaviour
             if (t > 1) { t = 1; dashIsActive = false; }
 
             transform.position = Vector3.Lerp(playerLocation, targetLocation, t);
+            dashEffect.transform.position = transform.position;
 
             yield return null;
         }
