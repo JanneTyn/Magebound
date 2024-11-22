5daf8d805ae36aff5b9c2030dc7bfbbf4cb264b1
RSakuras <ahmad.dlvan@gmail.com>
2024-11-22 12:28:24 +0200

    Placeholdery main menu

---
+++ Assets/Scripts/UI/MainMenu/MainMenuButtons.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuButtons : MonoBehaviour
+{
+    public GameObject[] toEnable;
+    bool enableDisableIsActive;
+
+    public void StartButton()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void SupriseButton()
+    {
+        Application.OpenURL("https://shattereddisk.github.io/rickroll/rickroll.mp4");
+    }
+
+    public void SettingsButton()
+    {
+
+    }
+
+    public void QuitButton()
+    {
+        if(!enableDisableIsActive)
+        {
+            StartCoroutine(EnableThenDisable());
+        }
+    }
+
+    IEnumerator EnableThenDisable()
+    {
+        enableDisableIsActive = true;
+
+        foreach (var go in toEnable)
+        {
+            go.SetActive(true);
+        }
+
+        yield return new WaitForSeconds(0.5f);
+
+        foreach (var go in toEnable)
+        {
+            go.SetActive(false);
+        }
+
+        enableDisableIsActive = false;
+    }
+}
