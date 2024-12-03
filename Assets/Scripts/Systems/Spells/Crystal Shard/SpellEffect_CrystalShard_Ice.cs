c7f9bc222be7aa5fa218811956c586c7c8d0cf54
Janne <jannetyni98@gmail.com>
2024-12-03 13:42:41 +0200

    ice shard WIP

---
+++ Assets/Scripts/Systems/Spells/Crystal Shard/SpellEffect_CrystalShard_Ice.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using UnityEngine;
+
+public class SpellEffect_CrystalShard_Ice : SpellEffect_WorldEffect
+{
+    public float shardDuration = 3;
+    public float explosionSize = 5;
+    float timer = 0;
+    float secs = 0;
+    bool delayPassed = false;
+    SphereCollider areaEffect;
+    MeshRenderer areaRender;
+    [SerializeField] private Material red;
+    [SerializeField] private float statusDuration = 1;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        areaEffect = GetComponentInChildren<SphereCollider>();
+        areaRender = GetComponentInChildren<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {       
+        if (timer < shardDuration)
+        {
+            delayPassed = true;
+            //areaEffect.enabled = true;
+
+            if (timer > secs)
+            {
+                Collider[] enemies = Physics.OverlapSphere(transform.position, explosionSize);
+
+                foreach (Collider enemy in enemies)
+                {
+                    if (enemy.TryGetComponent<DamageSystem>(out DamageSystem dmg))
+                    {
+                        if (enemy.tag == "Enemy") dmg.CalculateDamage(GetDamage(), true, GetStatusID(), 1, GetElementID());
+                    }
+                    else
+                    {
+                        Debug.Log("Damagesystem not found");
+                    }
+                }
+                secs++;
+            }
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        timer += Time.deltaTime;
+    }
+
+    IEnumerator DestroyAfterEffect()
+    {
+        yield return new WaitForSeconds(2);
+        Destroy(gameObject);
+    }
+}
--- Assets/Scripts/Systems/Spells/SpellShard.cs
+++ Assets/Scripts/Systems/Spells/SpellShard.cs
@@ -7,17 +7,6 @@ public class SpellShard : MonoBehaviour
     public GameObject CrystalShardIcePrefab;
     public GameObject CrystalShardThunderPrefab;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     public void InitializeShard(Vector3 playerLocation, Vector3 targetLocation, int element)
     {
@@ -27,11 +16,10 @@ public class SpellShard : MonoBehaviour
                 GameObject shard = Instantiate(CrystalShardFirePrefab, targetLocation, Quaternion.identity);
                 break;
             case 2:
-                GameObject iceshard = Instantiate(CrystalShardIcePrefab, playerLocation, Quaternion.identity);
-                iceshard.GetComponent<SpellEffect_Explosion_Ice>().SetProjectileDirection(targetLocation, playerLocation);
+                GameObject iceshard = Instantiate(CrystalShardIcePrefab, targetLocation, Quaternion.identity);
                 break;
             case 3:
-                GameObject thundershard = Instantiate(CrystalShardThunderPrefab, playerLocation, Quaternion.identity);
+                GameObject thundershard = Instantiate(CrystalShardThunderPrefab, targetLocation, Quaternion.identity);
                 thundershard.GetComponent<SpellEffect_Explosion_Thunder>().SetProjectileDirection(targetLocation, playerLocation);
                 break;
         }
